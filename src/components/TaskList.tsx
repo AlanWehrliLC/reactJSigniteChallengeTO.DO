c85287ae3def49e121762d25b5e4626c7d7b53d8
AlanWehrliLC <wehrlialan1@gmail.com>
2021-11-02 17:11:04 -0300

    Correct tests and challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -57,7 +57,7 @@ export function TaskList(){
             onChange={(e)=> setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" onClick={handleCreateNewTask}>
+          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
           </button>
         </div>
@@ -68,7 +68,7 @@ export function TaskList(){
 
         {tasks.map(task => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} >
+              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
                   <input 
                     type="checkbox"
@@ -82,6 +82,7 @@ export function TaskList(){
               </div>
 
               <button type="button" 
+              data-testid="remove-task-button"
               onClick={() => handleRemoveTask(task.id)}>
                 <FiTrash size={16}/>
               </button>
